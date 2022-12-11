ffa0b0662edbc73f0346517c0f60b26d3169ba5b
elkofy <nassimdaikh71@gmail.com>
2022-12-11 15:53:53 +0100

    fix: cors options

--- server/index.js
+++ server/index.js
@@ -9,6 +9,24 @@ const options = {
 
 const io = require("socket.io")(httpServer, options);
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://roland-gamos-git-master-elkofy.vercel.app/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.get("/", (req, res) => {
 
   res.send("<h1>Rollad Gamos Api</h1>");

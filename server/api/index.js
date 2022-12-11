f7e53be0a55d0dd90ce9a5a15149d21dc6f8afc1
elkofy <nassimdaikh71@gmail.com>
2022-12-11 15:22:10 +0100

    hotfix

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ const { v4 } = require('uuid');
 app.get('/api', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.send(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    res.send(`Hello!</a>`);
   });
 
   

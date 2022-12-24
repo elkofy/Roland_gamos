2a55c96bf62cc997cf6a85926d27e76af637d7b4
elkofy <nassimdaikh71@gmail.com>
2022-12-24 10:53:17 +0100

    deploy: chnaged api endpoint

--- rg_app/src/context/socket.js
+++ rg_app/src/context/socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 import React from "react";
-const ENDPOINT = 'https://roland-gamos-2g36.vercel.app/';
+const ENDPOINT = 'https://roland-gamos-api.herokuapp.com/';
 
 export const socket = socketio.connect(ENDPOINT);
 export const SocketContext = React.createContext();
